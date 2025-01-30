a23d778045bfe352fa37077c53a1c22ede0c0589
BugHunter-X <askzenai@gmail.com>
2025-01-30 21:58:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,12 @@
+function foo(a, b) {
+  if (a === b) {
+    return true;
+  } else if (a > b) {
+    return foo(a - b, b);
+  } else {
+    return foo(a, b - a);
+  }
+}
+
+console.log(foo(10, 2)); // Expected output: false, Actual output: true
+console.log(foo(15, 5)); // Expected output: true, Actual output: true
